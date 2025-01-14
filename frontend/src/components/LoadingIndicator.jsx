6c9cb6ac4177a60ad27b3f5f63ec65169d005f17
Samuel Buriticá <sbsr95@hotmail.com>
2025-01-14 11:10:28 -0500

    First commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+import "../styles/LoadingIndicator.css";
+
+const Loadingindicator = () => {
+    return (
+        <div className="loading-container">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default Loadingindicator;
